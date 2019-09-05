f4cb959726eac1d56d6a934c596718072b8a8e91
Michael Yin <michaelyinwork@gmail.com>
2019-09-05 15:06:31 +1000

    use material-ui instead of bootstrap

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
-}
+const Layout = props => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Job Shop Collaction
+          </Typography>
+          <Button color="inherit">About</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        {props.children}
+      </Container>
+    </div>
+  );
+};
+
+export default Layout
