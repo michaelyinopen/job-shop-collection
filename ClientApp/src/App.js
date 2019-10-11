a1ddcfb1f4e78132e0f7dfdd78602e0ccbc20f82
Michael Yin <michaelyinwork@gmail.com>
2019-10-11 14:31:54 +1100

    About page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Layout from './components/Layout';
 import { Home } from './components/Home';
+import About from './components/About';
 import * as fromRoutePaths from './routePaths';
 import JobShopCollection from './components/JobShopCollection';
 import JobSets from './components/JobSets';
@@ -16,6 +17,7 @@ export default class App extends Component {
         <JobShopCollection>
           <Switch>
             <Route exact path={fromRoutePaths.home} component={Home} />
+            <Route exact path={fromRoutePaths.about} component={About} />
             <Route exact path={fromRoutePaths.jobSets} component={JobSets} />
             <Route path={fromRoutePaths.jobSet} component={JobSets} />
             <Route path={fromRoutePaths.jobSetEditor} component={JobSetEditor} />
